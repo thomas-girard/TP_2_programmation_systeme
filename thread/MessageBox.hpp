9385bad67b40d1c45233ffbb52f4c9d9aa28010f
thomas-girard <t.girard912@laposte.net>
2021-10-11 11:52:47 +0200

    fin partie 3

--- thread/MessageBox.hpp
+++ thread/MessageBox.hpp
@@ -18,7 +18,7 @@ std::mutex my_mutex;
 int nb_data=0;
 
 std::condition_variable boite_vide;
-
+std::condition_variable boite_pleine;
 /*
  * FIFO d'echange de messages entre producteurs et consommateurs
  * Version pour synchronisation entre threads
@@ -29,21 +29,32 @@ public:
 
         std::unique_lock<std::mutex> my_lock(my_mutex);
 
-        while (nb_data <= box_size_) {
+        if (nb_data < box_size_) {
             // std::this_thread::sleep_for (std::chrono::seconds(1));
             basic_put( message );
             nb_data+=1;
+            std::cout << "je remplit la boite : " << nb_data << std::endl;
             boite_vide.notify_all();
         }
-        // TODO : ajouter les mecanismes de synchronisation
+
+        else {
+            std::cout << "wait notif de boite_pleine" << std::endl;
+            boite_pleine.wait(my_lock);
+        }
     }
 
     int get() {
         // TODO : ajouter les mecanismes de synchronisation
         std::unique_lock<std::mutex> my_lock(my_mutex);
-        boite_vide.wait(my_lock);
+        while (nb_data <= 0) {
+            std::cout << "wait notif de boite_vide" << std::endl;
+            boite_vide.wait(my_lock);
+        }
         int message{ basic_get() };
         nb_data-=1;
+        std::cout << "je vide la boite : " << nb_data << std::endl;
+        boite_pleine.notify_all();
+
         return message;
     }
 private:
--- thread/Threads.cpp
+++ thread/Threads.cpp
@@ -39,17 +39,33 @@ int main() {
     Random generator{ 50 };
     int alea = generator();
     MessageBox boite_aux_lettres;
-    Producer producteur(1, boite_aux_lettres, generator, 20);
-    Consumer consommateur(1, boite_aux_lettres, generator, 20);
+
+    Producer producteur_1(1, boite_aux_lettres, generator, 10);
+    Producer producteur_2(1, boite_aux_lettres, generator, 10);
+    Producer producteur_3(1, boite_aux_lettres, generator, 10);
+    Producer producteur_4(1, boite_aux_lettres, generator, 10);
+
+    Consumer consommateur_1(1, boite_aux_lettres, generator, 20);
+    Consumer consommateur_2(1, boite_aux_lettres, generator, 20);
 
     //producteur.operator()();
     //pthread_cond_init( , NULL );
 
-    std::thread c(consommateur);
-    std::thread p(producteur);
+    std::thread c_1(consommateur_1);
+    std::thread c_2(consommateur_2);
 
-    c.join();
-    p.join();
+    std::thread p_1(producteur_1);
+    std::thread p_2(producteur_2);
+    std::thread p_3(producteur_3);
+    std::thread p_4(producteur_4);
+
+    c_1.join();
+    c_2.join();
+
+    p_1.join();
+    p_2.join();
+    p_3.join();
+    p_4.join();
 
     return 0;
 }
